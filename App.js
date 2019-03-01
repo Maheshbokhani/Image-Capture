0308ac4a3816af18b856bf42bdb892eca6380219
Maheshbokhani <mahesh.bokhani@bacancytechnology.com>
2019-03-01 18:48:36 +0530

    update

---
+++ App.js
@@ -0,0 +1,85 @@
+import React from 'react'
+import { View, Image, StyleSheet, TouchableOpacity, ScrollView, Text } from 'react-native'
+import ImagePicker from 'react-native-image-picker'
+
+export default class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            imageSource: { uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/No_image_available_400_x_600.svg/2000px-No_image_available_400_x_600.svg.png' },
+        }
+    }
+
+    selectPhoto = () => {
+        const options = {
+            title: 'Select a option',
+            takePhotoButtonTitle: 'Take a photo',
+            chooselibraryButtonTitle: 'Choose from Gallery',
+            quality: 1
+        }
+
+        ImagePicker.showImagePicker(options, (response) => {
+            console.log('Respose = ', response);
+
+            if (response.didCancel) {
+                console.log('User cancelled image picker');
+            }
+
+            else if (response.error) {
+                console.log('ImagePicker Error: ', response.error);
+            }
+            else {
+                let source = { uri: response.uri };
+                this.setState({
+                    imageSource: source
+                })
+            }
+        })
+    }
+    render() {
+        return (
+            <ScrollView style={{ backgroundColor: '#fff' }}>
+
+                <View style={styles.container}>
+
+                    <View style={{ alignItems: 'center', height: 60, backgroundColor: 'lightblue', flex: 0.1, justifyContent: 'center' }}>
+                        <Text style={{ fontSize: 23, fontWeight: 'bold' }}>Image Capture  </Text>
+                    </View>
+
+                    <View style={{ flex: 0.9, alignItems: 'center' }}>
+
+                        <Image
+                            style={{ width: 300, height: 300, marginTop: 20, marginBottom: 5 }}
+                            source={this.state.imageSource}
+                        />
+
+                        <TouchableOpacity style={styles.button} onPress={this.selectPhoto.bind(this)}>
+
+                            <Text style={{ fontSize: 18, fontWeight: 'bold' }}> Click Here </Text>
+
+                        </TouchableOpacity>
+
+                    </View>
+
+                </View>
+            </ScrollView>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff'
+    },
+    button: {
+        width: 220,
+        height: 50,
+        borderRadius: 7,
+        marginTop: 20,
+        marginBottom: 20,
+        backgroundColor: 'lightblue',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+})
